3e34e900047e4b357301dbaf7a98eb467dff2be3
MasjideFathima <masjidfathimajaqh@gmail.com>
2026-09-16 23:01:53 +0530

    Apply soft theme to the dashboard

--- mfjaqh/pages/1_Dashboard.py
+++ mfjaqh/pages/1_Dashboard.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 from utils.auth import require_login
 from utils.db import get_transactions
+from utils.style import apply_soft_theme
 
+apply_soft_theme()
 require_login()
 st.title("📊 Dashboard")
 
